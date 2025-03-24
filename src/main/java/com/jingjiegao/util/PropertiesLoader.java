40baa65cae39a7730e208fb032cca20cf6147d89
Jingjie Gao <jgao2@madisoncollege.edu>
2025-03-24 00:37:04 -0500

    Create the PropertiesLoader class to load swapi properties

--- src/main/java/com/jingjiegao/util/PropertiesLoader.java
+++ src/main/java/com/jingjiegao/util/PropertiesLoader.java
@@ -1,4 +1,24 @@
 package com.jingjiegao.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class PropertiesLoader {
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream input = PropertiesLoader.class.getClassLoader().getResourceAsStream("swapi.properties")) {
+            if (input == null) {
+                throw new RuntimeException("Cannot find swapi.properties file.");
+            }
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load swapi.properties", e);
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
 }
