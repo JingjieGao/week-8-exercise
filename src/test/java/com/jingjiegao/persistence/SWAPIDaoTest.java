2368c9c91e2b1e3390fff255893de7029962386e
Jingjie Gao <jgao2@madisoncollege.edu>
2025-03-24 00:42:38 -0500

    Create the test for SWAPIDao class

--- src/test/java/com/jingjiegao/persistence/SWAPIDaoTest.java
+++ src/test/java/com/jingjiegao/persistence/SWAPIDaoTest.java
@@ -1,4 +1,14 @@
+package com.jingjiegao.persistence;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SWAPIDaoTest {
-  
+
+    @Test
+    void getPlanetSuccess() {
+        SWAPIDao dao = new SWAPIDao();
+        assertEquals("Tatooine",dao.getPlanet().getName());
+    }
 }
