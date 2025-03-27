96b101182c80fe9e6c1a576b68d632f8fff75b5f
Jingjie Gao <jgao2@madisoncollege.edu>
2025-03-26 23:27:08 -0500

    Create a test for PSAPIDao to check if the data is fetched from the pet store API successfully

--- src/test/java/com/jingjiegao/persistence/PSAPIDaoTest.java
+++ src/test/java/com/jingjiegao/persistence/PSAPIDaoTest.java
@@ -1,4 +1,20 @@
+package com.jingjiegao.persistence;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * The type Psapi dao test.
+ */
 class PSAPIDaoTest {
-  
+
+    /**
+     * Gets order success.
+     */
+    @Test
+    void getOrderSuccess() {
+        PSAPIDao dao = new PSAPIDao();
+        assertEquals("placed", dao.getOrder().getStatus());
+    }
 }
