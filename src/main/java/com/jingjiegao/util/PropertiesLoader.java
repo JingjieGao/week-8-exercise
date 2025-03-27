20f77b798febef561316993cb2c5e18fb98906c8
Jingjie Gao <jgao2@madisoncollege.edu>
2025-03-26 23:19:00 -0500

    Modify the PropertiesLoader for loading the petstore.properties file

--- src/main/java/com/jingjiegao/util/PropertiesLoader.java
+++ src/main/java/com/jingjiegao/util/PropertiesLoader.java
@@ -4,20 +4,29 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+/**
+ * The type Properties loader.
+ */
 public class PropertiesLoader {
     private static final Properties properties = new Properties();
 
     static {
-        try (InputStream input = PropertiesLoader.class.getClassLoader().getResourceAsStream("swapi.properties")) {
+        try (InputStream input = PropertiesLoader.class.getClassLoader().getResourceAsStream("petstore.properties")) {
             if (input == null) {
-                throw new RuntimeException("Cannot find swapi.properties file.");
+                throw new RuntimeException("Cannot find petstore.properties file.");
             }
             properties.load(input);
         } catch (IOException e) {
-            throw new RuntimeException("Failed to load swapi.properties", e);
+            throw new RuntimeException("Failed to load petstore.properties", e);
         }
     }
 
+    /**
+     * Gets property.
+     *
+     * @param key the key
+     * @return the property
+     */
     public static String getProperty(String key) {
         return properties.getProperty(key);
     }
